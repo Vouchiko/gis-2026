f273db9320dc2ad8ab382724359c91b71ac57182
Vouchiko <vovakarpov2017@yandex.ru>
2026-04-25 15:04:42 +0400

    lab_2

---
+++ lab_2/client/src/main.js
@@ -0,0 +1,40 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import ImageLayer from 'ol/layer/Image';
+import ImageWMS from 'ol/source/ImageWMS';
+import { fromLonLat } from 'ol/proj';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({ source: new OSM() }),
+    new ImageLayer({
+      source: new ImageWMS({
+        url: 'http://localhost:8080/geoserver/gis/wms',
+        params: { LAYERS: 'gis:buildings', TILED: true },
+        serverType: 'geoserver'
+      })
+    }),
+    new ImageLayer({
+      source: new ImageWMS({
+        url: 'http://localhost:8080/geoserver/gis/wms',
+        params: { LAYERS: 'gis:roads', TILED: true },
+        serverType: 'geoserver'
+      })
+    }),
+    new ImageLayer({
+      source: new ImageWMS({
+        url: 'http://localhost:8080/geoserver/gis/wms',
+        params: { LAYERS: 'gis:poi', TILED: true },
+        serverType: 'geoserver'
+      })
+    })
+  ],
+  view: new View({
+    center: fromLonLat([50.335, 53.557]),
+    zoom: 14
+  })
+});
